29e404b7d79d7b45607a3ed4f6f282220ad715f4
NSergeiV <ttniseva@yandex.ru>
2021-12-03 02:34:32 +0300

    Выполнил задание - 1.9. Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
 import {createTripInfoTemplate} from './view/trip-main-info-view.js';
+import {createTripMainControlsTemplate} from './view/trip-main-trip-controls-view.js';
+import {createFormTripControlsTemplate} from './view/form-trip-sort-trip-events-view.js';
+import {createFormEditPointTemplate} from './view/edit-point-view.js';
+import {createTripEventsListTemplate} from './view/trip-events-list-view.js';
+import {createRoutePointTemplate} from './view/route-point-view.js';
 import {renderTemplate, RenderPosition} from './utils/render.js';
 
+const TASK_COUNT = 3;
+
 const headerMenu = document.querySelector('.trip-main');
+const buttonNewEvent = headerMenu.querySelector('.trip-main__event-add-btn');
+const tripEvents = document.querySelector('.trip-events');
+const titleTripEvents = tripEvents.querySelector('h2');
 
 renderTemplate(headerMenu, createTripInfoTemplate(), RenderPosition.AFTERBEGIN );
+renderTemplate(buttonNewEvent, createTripMainControlsTemplate(), RenderPosition.BEFOREBEGIN);
+renderTemplate(titleTripEvents, createFormTripControlsTemplate(), RenderPosition.AFTEREND);
+
+const formTripSort = tripEvents.querySelector('.trip-events__trip-sort');
+
+renderTemplate(formTripSort, createTripEventsListTemplate(), RenderPosition.AFTEREND);
+
+const pointList = tripEvents.querySelector('.trip-events__list');
+
+for (let i = 0; i < TASK_COUNT; i++) {
+  renderTemplate(pointList, createRoutePointTemplate(), RenderPosition.AFTERBEGIN);
+}
+
+renderTemplate(pointList, createFormEditPointTemplate(), RenderPosition.AFTERBEGIN);
