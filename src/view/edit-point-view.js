27050faea69e03638de1e6a0ed9c29feee745f70
NSergeiV <ttniseva@yandex.ru>
2022-01-25 15:26:38 +0300

    Выполнил основное задание - 6.5. Обновление века (часть 1)

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -1,4 +1,4 @@
-import AbstractView from './abstract-view.js';
+import SmartView from './smart-view.js';
 
 const BLANK_POINT = {
   id: null,
@@ -21,16 +21,21 @@ const BLANK_POINT = {
   isFavorite: false,
 };
 
+const findLastWord = (str) => {
+  const arrayString = str.split(' ');
+  return arrayString[arrayString.length -1];
+};
+
 const createEventOffer = (offers, isOfferLength) => (
   `${isOfferLength ? `<section class="event__section  event__section--offers">
      <h3 class="event__section-title  event__section-title--offers">Offers</h3>
      <div class="event__available-offers">
      ${offers.map((offer) => `<div class="event__offer-selector">
-        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${Object.keys(offer)}-1" type="checkbox" name="event-offer-${Object.keys(offer)}" checked>
-        <label class="event__offer-label" for="event-offer-${Object.keys(offer)}-1">
-          <span class="event__offer-title">${Object.keys(offer)}</span>
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${findLastWord(offer.title)}-1" type="checkbox" name="event-offer-${findLastWord(offer.title)}" ${offer.isActive ? 'checked' : ''} data-id = "${offer.id}">
+        <label class="event__offer-label" for="event-offer-${findLastWord(offer.title)}-1">
+          <span class="event__offer-title">${offer.title}</span>
           &plus;&euro;&nbsp;
-          <span class="event__offer-price">${Object.values(offer)}</span>
+          <span class="event__offer-price">${offer.price}</span>
         </label>
       </div>`).join('')}
     </div>
@@ -59,12 +64,12 @@ const createEventDestination = (description, eventPhotos, isDescriptionLength, i
 
 const createFormEditPointTemplate = (data) => {
 
-  const {eventIcon, eventType, eventOffer, description, eventPhoto, eventCity, eventPrice, isOfferLength, isDescriptionLength, isEventPhoto, isEventType} = data;
+  const {eventIcon, eventType, eventOffer, description, eventPhoto, eventCity, eventPrice, isOfferLength, isOfferClick, isDescriptionLength, isEventPhoto, isEventType, isEventPrice, isEventCity} = data;
 
   const repeatingOffer = createEventOffer(eventOffer, isOfferLength);
   const destination = createEventDestination(description, eventPhoto, isDescriptionLength, isEventPhoto);
 
-  const isSubmitDisabled = isEventType;
+  const isSubmitDisabled = isEventType && isEventPrice && isOfferClick && isEventCity;
 
   return `<form class="event event--edit" action="#" method="post">
       <header class="event__header">
@@ -152,7 +157,7 @@ const createFormEditPointTemplate = (data) => {
             <span class="visually-hidden">Price</span>
             &euro;
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value=${eventPrice}>
+          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value=${eventPrice} autocomplete="off">
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit" ${isSubmitDisabled ? 'disabled' : ''}>Save</button>
@@ -168,9 +173,8 @@ const createFormEditPointTemplate = (data) => {
     </form>`;
 };
 
-// ${isSubmitDisabled ? 'disabled' : ''}
-
-export default class FormEditPointView extends AbstractView {
+export default class FormEditPointView extends SmartView {
+  #arrayId = [];
 
   constructor(routePoint = BLANK_POINT) {
     super();
@@ -180,7 +184,38 @@ export default class FormEditPointView extends AbstractView {
     this._testTypePoint = [
       {
         eventType: 'Taxi',
-        eventOffer: [{'Add luggage': 30}, {'Switch to comfort class': 100}, {'Add meal': 15}, {'Choose seats': 5}, {'Travel by train': 40}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          },
+          {
+            id: 2,
+            title: 'Switch to comfort',
+            price: 100,
+            isActive: true
+          },
+          {
+            id: 3,
+            title: 'Add meal',
+            price: 15,
+            isActive: true
+          },
+          {
+            id: 4,
+            title: 'Choose seats',
+            price: 5,
+            isActive: true
+          },
+          {
+            id: 5,
+            title: 'Travel by train',
+            price: 40,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/taxi.png',
       },
       {
@@ -190,27 +225,128 @@ export default class FormEditPointView extends AbstractView {
       },
       {
         eventType: 'Train',
-        eventOffer: [{'Add luggage': 30}, {'Switch to comfort class': 100}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          },
+          {
+            id: 2,
+            title: 'Switch to comfort',
+            price: 100,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/train.png',
       },
       {
         eventType: 'Ship',
-        eventOffer: [{'Add luggage': 30}, {'Switch to comfort class': 100}, {'Add meal': 15}, {'Choose seats': 5}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          },
+          {
+            id: 2,
+            title: 'Switch to comfort',
+            price: 100,
+            isActive: true
+          },
+          {
+            id: 3,
+            title: 'Add meal',
+            price: 15,
+            isActive: true
+          },
+          {
+            id: 4,
+            title: 'Choose seats',
+            price: 5,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/ship.png',
       },
       {
         eventType: 'Drive',
-        eventOffer: [{'Add luggage': 30}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/drive.png',
       },
       {
         eventType: 'Flight',
-        eventOffer: [{'Add luggage': 30}, {'Switch to comfort class': 100}, {'Add meal': 15}, {'Choose seats': 5}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          },
+          {
+            id: 2,
+            title: 'Switch to comfort',
+            price: 100,
+            isActive: true
+          },
+          {
+            id: 3,
+            title: 'Add meal',
+            price: 15,
+            isActive: true
+          },
+          {
+            id: 4,
+            title: 'Choose seats',
+            price: 5,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/flight.png',
       },
       {
         eventType: 'Check-in',
-        eventOffer: [{'Add luggage': 30}, {'Switch to comfort class': 100}, {'Add meal': 15}, {'Choose seats': 5}, {'Travel by train': 40}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          },
+          {
+            id: 2,
+            title: 'Switch to comfort',
+            price: 100,
+            isActive: true
+          },
+          {
+            id: 3,
+            title: 'Add meal',
+            price: 15,
+            isActive: true
+          },
+          {
+            id: 4,
+            title: 'Choose seats',
+            price: 5,
+            isActive: true
+          },
+          {
+            id: 5,
+            title: 'Travel by train',
+            price: 40,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/check-in.png',
       },
       {
@@ -220,12 +356,50 @@ export default class FormEditPointView extends AbstractView {
       },
       {
         eventType: 'Restaurant',
-        eventOffer: [{'Add luggage': 30}, {'Switch to comfort class': 100}, {'Add meal': 15}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          },
+          {
+            id: 2,
+            title: 'Switch to comfort',
+            price: 100,
+            isActive: true
+          },
+          {
+            id: 3,
+            title: 'Add meal',
+            price: 15,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/restaurant.png',
       },
       {
         eventType: 'Transport',
-        eventOffer: [{'Add luggage': 30}, {'Switch to comfort class': 100}, {'Add meal': 15}],
+        eventOffer: [
+          {
+            id: 1,
+            title: 'Add luggage',
+            price: 30,
+            isActive: true
+          },
+          {
+            id: 2,
+            title: 'Switch to comfort',
+            price: 100,
+            isActive: true
+          },
+          {
+            id: 3,
+            title: 'Add meal',
+            price: 15,
+            isActive: true
+          }
+        ],
         eventIcon: 'img/icons/transport.png',
       },
     ];
@@ -264,20 +438,47 @@ export default class FormEditPointView extends AbstractView {
   }
 
   #setInnerHandlers = () => {
-    console.log('ПРИХОДИТ');
     this.element.querySelector('fieldset').addEventListener('click', this.#formSelectTypePoint);
     this.element.querySelector('.event__input--destination').addEventListener('change', this.#formSelectDestination);
+
+    if (this._data.eventOffer.length !== 0) {
+      this.element.querySelector('.event__section--offers').addEventListener('click', this.#formSelectOffers);
+    }
+
     const inputPrice = this.element.querySelector('.event__input--price');
 
-    inputPrice.addEventListener('keyup', () => {
-      const valuePrice = inputPrice.value;
+    inputPrice.addEventListener('input', () => {
+      let valuePrice = inputPrice.value;
+      const priceReplacement = (this._testData.eventPrice).toString() === valuePrice;
+
       if (valuePrice.match(/[^\d]+/g)) {
-        inputPrice.setCustomValidity('Нужны цифры');
+        inputPrice.setCustomValidity('Нужно целое положительное число');
+        this.element.querySelector('.event__save-btn').setAttribute('disabled', '');
       } else if (valuePrice === '') {
         inputPrice.setCustomValidity('Заполните пожалуйста');
+        this.element.querySelector('.event__save-btn').setAttribute('disabled', '');
       } else {
         inputPrice.setCustomValidity('');
+
+        if (priceReplacement) {
+          if (this._data.isEventType && this._data.isOfferClick && this._data.isEventCity) {
+            this.element.querySelector('.event__save-btn').setAttribute('disabled', '');
+          }
+
+          valuePrice = this._testData.eventPrice;
+          this.updateData({
+            isEventPrice: priceReplacement,
+            eventPrice: valuePrice,
+          }, true);
+          return;
+        }
+        this.element.querySelector('.event__save-btn').removeAttribute('disabled');
+        this.updateData({
+          isEventPrice: priceReplacement,
+          eventPrice: valuePrice,
+        }, true);
       }
+
       inputPrice.reportValidity();
     });
 
@@ -285,18 +486,36 @@ export default class FormEditPointView extends AbstractView {
     const cities = this._testDestination.map((item) => item.city);
 
     inputDestination.addEventListener('input', () => {
-      const destinationValue = inputDestination.value;
-      if (destinationValue.length !== 0) {
-        cities.forEach((city) => {
-          if (city.toLowerCase().includes(destinationValue.toLowerCase(), 1)) {
-            inputDestination.setCustomValidity('Выберите из списка.');
-          } else {
-            inputDestination.setCustomValidity('Такого города нет в списке.');
-          }
-        });
-      } else {
+      let destinationValue = inputDestination.value;
+      const destinationReplacement = this._testData.eventCity === destinationValue;
+
+      if (destinationValue.length === 0) {
+        this.element.querySelector('.event__save-btn').setAttribute('disabled', '');
         inputDestination.setCustomValidity('Введите название города.');
+      } else if (cities.includes(destinationValue, 1) === false) {
+        inputDestination.setCustomValidity('Такого города нет в списке.');
+      } else {
+        inputDestination.setCustomValidity('');
       }
+
+      if (destinationReplacement) {
+        if (this._data.isEventType && this._data.isOfferClick && this._data.isEventPrice) {
+          this.element.querySelector('.event__save-btn').setAttribute('disabled', '');
+        }
+
+        destinationValue = this._testData.eventCity;
+        this.updateData({
+          isEventCity: destinationReplacement,
+          eventCity: destinationValue,
+        }, true);
+        return;
+      }
+
+      this.updateData({
+        isEventCity: destinationReplacement,
+        eventCity: destinationValue,
+      }, true);
+
       inputDestination.reportValidity();
     });
   }
@@ -307,23 +526,13 @@ export default class FormEditPointView extends AbstractView {
     const nameIconType = evt.target.closest('div').querySelector('input').value;
     const nameTypeChoice = evt.target.textContent;
 
-    console.log(nameIconType);
-    console.log(nameTypeChoice);
-
-    console.log(this._testData.eventType);
-
-    if (this._testData.eventType !== nameTypeChoice) {
-      console.log(true);
-    } else {
-      console.log(false);
-    }
-
-    const typeReplacement = this._testData.eventType !== nameTypeChoice ? false : true;
-    console.log(typeReplacement);
+    const typeReplacement = this._testData.eventType === nameTypeChoice;
 
     this.element.querySelector('.event__type-output').textContent = nameTypeChoice;
     this.element.querySelector('.event__type-icon').src = `img/icons/${nameIconType}.png`;
     this.element.querySelector('.event__type-list').style.display = 'none';
+
+    this._testTypePoint.forEach((data) => data.eventOffer.forEach((offer) => (offer.isActive = true)));
 
     const transportType = this._testTypePoint.filter((type) => type.eventType === nameTypeChoice);
 
@@ -334,6 +543,8 @@ export default class FormEditPointView extends AbstractView {
       eventType: transportType[0].eventType,
       eventOffer: transportType[0].eventOffer,
     });
+
+    this.#arrayId.splice(0, this.#arrayId.length);
   }
 
   #formSelectDestination = (evt) => {
@@ -350,6 +561,35 @@ export default class FormEditPointView extends AbstractView {
     });
   }
 
+  #formSelectOffers = (evt) => {
+    if (evt.target.matches('input[type="checkbox"]')) {
+      const offerId = evt.target.dataset.id;
+      const arrayOffers = this._data.eventOffer;
+      if (this.#arrayId.length === 0) {
+        this.#arrayId.push(offerId);
+      } else {
+        if (this.#arrayId.includes(offerId)) {
+          this.#arrayId = this.#arrayId.filter((item) => item !== offerId);
+        } else {
+          this.#arrayId.push(offerId);
+        }
+      }
+
+      const offerStatusChanged = this.#arrayId.length === 0;
+
+      if (arrayOffers[offerId -1].isActive) {
+        arrayOffers[offerId -1].isActive = false;
+      } else {
+        arrayOffers[offerId -1].isActive = true;
+      }
+
+      this.updateData({
+        isOfferClick: offerStatusChanged,
+        eventOffer: arrayOffers,
+      });
+    }
+  }
+
   #formHandler = (evt) => {
     evt.preventDefault();
     this._callback.formClick();
@@ -360,27 +600,10 @@ export default class FormEditPointView extends AbstractView {
     this._callback.formSubmit(FormEditPointView.parsePointToData(this._data));
   }
 
-  updateData = (update) => {
-    if (!update) {
-      return;
-    }
-
-    this._data = {...this._data, ...update};
-
-    this.updateElement();
-  }
-
-  updateElement = () => {
-    const prevElement = this.element;
-
-    const parent = prevElement.parentElement;
-    this.removeElement();
-
-    const newElement = this.element;
-
-    parent.replaceChild(newElement, prevElement);
-
-    this.restoreHandlers();
+  reset = (task) => {
+    this.updateData(
+      FormEditPointView.parsePointToData(task),
+    );
   }
 
   restoreHandlers = () => {
@@ -391,14 +614,25 @@ export default class FormEditPointView extends AbstractView {
 
   static parsePointToData = (point) => ({
     ...point,
+    isEventPrice: point.eventPrice !== null,
     isEventType: point.eventType !== null,
     isEventPhoto: point.eventPhoto !== null,
     isOfferLength: point.eventOffer.length !== 0,
+    isOfferClick: point.eventOffer.length !== 0,
     isDescriptionLength: point.description.length !== 0,
+    isEventCity: point.eventCity !== 0,
   })
 
   static parseDataToPoint = (data) => {
     const point = {...data};
+
+    if (!point.isEventCity) {
+      point.eventCity = null;
+    }
+
+    if (!point.isEventPrice) {
+      point.eventPrice = null;
+    }
 
     if (!point.isEventType) {
       point.eventType = null;
@@ -412,13 +646,20 @@ export default class FormEditPointView extends AbstractView {
       point.eventOffer = [];
     }
 
+    if (!point.isOfferClick) {
+      point.eventOffer = [];
+    }
+
     if (!point.isDescriptionLength) {
       point.description = '';
     }
 
     delete point.isEventType;
+    delete point.isEventCity;
+    delete point.isEventPrice;
     delete point.isEventPhoto;
     delete point.isOfferLength;
+    delete point.isOfferClick;
     delete point.isDescriptionLength;
 
     return point;
